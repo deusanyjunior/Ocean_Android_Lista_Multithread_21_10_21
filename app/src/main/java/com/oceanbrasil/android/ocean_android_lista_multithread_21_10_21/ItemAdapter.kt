d628e7fc5ab0e3fc9890af738a69404f4674c0e8
dj <dj@ime.usp.br>
2021-10-21 10:54:57 -0300

    Finalizando ItemAdapter, Criando Persona, Definindo ViewHolder

--- app/src/main/java/com/oceanbrasil/android/ocean_android_lista_multithread_21_10_21/ItemAdapter.kt
+++ app/src/main/java/com/oceanbrasil/android/ocean_android_lista_multithread_21_10_21/ItemAdapter.kt
@@ -1,22 +1,51 @@
 package com.oceanbrasil.android.ocean_android_lista_multithread_21_10_21
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+class ItemAdapter (
+    val itens: List<Persona>
+    ) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
+        fun bindView(item: Persona) {
+            val tvTitulo = itemView.findViewById<TextView>(R.id.tvTitulo)
+            val imagem = itemView.findViewById<ImageView>(R.id.imagem)
+            val btAcao = itemView.findViewById<Button>(R.id.btAcao)
 
+            tvTitulo.text = item.nome
+            // Com Atenas
+            Glide.with(itemView).load(item.imagemUrl).into(imagem)
+            // Com Sparta
+            // - Fazer o download da imagem (baixar a partir da URL)
+            // - Tratar problemas de download (atraso, reconexão, perda de bits)
+            // - Atribuir a imagem baixada à variável
+            // val imagemSparta = URL(item.imagemUrl)
+            //    ...
+            // imagem.setImageBitmap()...
+
+            btAcao.setOnClickListener {
+                Toast.makeText(itemView.context,item.acao,Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item,parent,false)
+
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = itens[position]
+        holder.bindView(item)
     }
 
     override fun getItemCount(): Int {
---
+++ app/src/main/java/com/oceanbrasil/android/ocean_android_lista_multithread_21_10_21/Persona.kt
@@ -0,0 +1,7 @@
+package com.oceanbrasil.android.ocean_android_lista_multithread_21_10_21
+
+data class Persona(
+    val nome: String,
+    val imagemUrl: String,
+    val acao: String
+)
