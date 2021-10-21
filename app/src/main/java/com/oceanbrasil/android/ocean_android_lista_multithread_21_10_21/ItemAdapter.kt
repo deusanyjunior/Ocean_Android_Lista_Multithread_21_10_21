79ebecfb4c984300d98e5b0dab35d9f3712351ca
dj <dj@ime.usp.br>
2021-10-21 10:15:53 -0300

    Adicionando ItemAdapter.kt

---
+++ app/src/main/java/com/oceanbrasil/android/ocean_android_lista_multithread_21_10_21/ItemAdapter.kt
@@ -0,0 +1,26 @@
+package com.oceanbrasil.android.ocean_android_lista_multithread_21_10_21
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/com/oceanbrasil/android/ocean_android_lista_multithread_21_10_21/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/android/ocean_android_lista_multithread_21_10_21/MainActivity.kt
@@ -2,10 +2,18 @@ package com.oceanbrasil.android.ocean_android_lista_multithread_21_10_21
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Layout Manager
+        val rvPessoas = findViewById<RecyclerView>(R.id.rvPessoas)
+        rvPessoas.layoutManager = LinearLayoutManager(this)
+
+        // Adapter
     }
 }
