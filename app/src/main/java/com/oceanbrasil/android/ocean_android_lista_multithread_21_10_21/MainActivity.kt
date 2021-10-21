06dc70006558febc6090929b263c38d72ec7144b
dj <dj@ime.usp.br>
2021-10-21 09:50:13 -0300

    Commit com o card implementado! pedido do Carlos Silva! =P

---
+++ app/src/main/java/com/oceanbrasil/android/ocean_android_lista_multithread_21_10_21/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.android.ocean_android_lista_multithread_21_10_21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
